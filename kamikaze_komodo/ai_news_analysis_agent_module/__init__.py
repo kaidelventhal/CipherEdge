5f0f51b2d645f962a4863a68dcf1384c2f26260d
kaidelventhal <kdelventhal03@gmail.com>
2025-06-04 20:05:42 -0400

    Fixed bugs

--- kamikaze_komodo/ai_news_analysis_agent_module/__init__.py
+++ kamikaze_komodo/ai_news_analysis_agent_module/__init__.py
@@ -1,3 +1,2 @@
 # kamikaze_komodo/ai_news_analysis_agent_module/__init__.py
 # This file makes the 'ai_news_analysis_agent_module' directory a Python package.
-logger_name = "KamikazeKomodo.ai_news_analysis_agent_module" # Satisfy linter
--- kamikaze_komodo/portfolio_constructor/__init__.py
+++ kamikaze_komodo/portfolio_constructor/__init__.py
@@ -1,3 +1,2 @@
 # kamikaze_komodo/portfolio_constructor/__init__.py
 # This file makes the 'portfolio_constructor' directory a Python package.
-logger_name = "KamikazeKomodo.portfolio_constructor" # Satisfy linter if file is empty
